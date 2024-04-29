402cf0592fd5dca94fe4debab418348ae7378364
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-29 15:28:58 -0300

    first commit

---
+++ src/main/java/com/tidcode/cruddemopart2/CruddemoPart2Application.java
@@ -0,0 +1,13 @@
+package com.tidcode.cruddemopart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddemoPart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoPart2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidcode/cruddemopart2/CruddemoPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidcode.cruddemopart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoPart2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
