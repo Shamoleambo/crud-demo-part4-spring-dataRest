c115bdd794b41491b3f7f4734278c15dd63c5768
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-30 08:23:50 -0300

    add endpoint to get single employee

--- src/main/java/com/tidcode/cruddemopart2/rest/EmployeeRestController.java
+++ src/main/java/com/tidcode/cruddemopart2/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,17 @@ public class EmployeeRestController {
 	@GetMapping("/employees")
 	public List<Employee> findAll() {
 		return this.employeeService.findAll();
+	}
+
+	@GetMapping("/employees/{employeeId}")
+	public Employee findById(@PathVariable int employeeId) {
+		Employee employee = this.employeeService.findById(employeeId);
+
+		if (employee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+
+		return employee;
 	}
 
 }
