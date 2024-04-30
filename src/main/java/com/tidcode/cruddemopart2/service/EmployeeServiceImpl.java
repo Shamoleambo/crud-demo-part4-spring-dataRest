e3ecedd40ec9bee4e39d3fc68da36a3bab24f3e3
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-30 07:36:11 -0300

    implement EmployeeService into EmployeeServiceImpl class

---
+++ src/main/java/com/tidcode/cruddemopart2/service/EmployeeServiceImpl.java
@@ -0,0 +1,26 @@
+package com.tidcode.cruddemopart2.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.tidcode.cruddemopart2.dao.EmployeeDAO;
+import com.tidcode.cruddemopart2.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	private EmployeeDAO employeeDAO;
+
+	@Autowired
+	public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO employeeDAO) {
+		this.employeeDAO = employeeDAO;
+	}
+
+	@Override
+	public List<Employee> findAll() {
+		return this.employeeDAO.findAll();
+	}
+}
