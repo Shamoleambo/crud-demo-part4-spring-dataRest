cdd87bf6dd36ea04690bb5a6b4ba8199160f738e
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-30 12:46:18 -0300

    change default endpoint path

--- src/main/java/com/tidcode/cruddemopart2/dao/EmployeeRepository.java
+++ src/main/java/com/tidcode/cruddemopart2/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.tidcode.cruddemopart2.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.tidcode.cruddemopart2.entity.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
