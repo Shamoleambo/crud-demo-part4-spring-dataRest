446c8599a1e18629b3f283d01b52108ba5ff0f40
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-30 11:48:51 -0300

    add endpoint to update an employee

--- src/main/java/com/tidcode/cruddemopart2/rest/EmployeeRestController.java
+++ src/main/java/com/tidcode/cruddemopart2/rest/EmployeeRestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +45,12 @@ public class EmployeeRestController {
 	@PostMapping("/employees")
 	public Employee addEmployee(@RequestBody Employee employee) {
 		employee.setId(0);
+		Employee dbEmployee = this.employeeService.save(employee);
+		return dbEmployee;
+	}
+	
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee employee) {
 		Employee dbEmployee = this.employeeService.save(employee);
 		return dbEmployee;
 	}
