469aa546077b36b9ab9fade95abbeea9d7460f5f
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-30 07:31:25 -0300

    create the EmployeeService interface

---
+++ src/main/java/com/tidcode/cruddemopart2/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.tidcode.cruddemopart2.service;
+
+import java.util.List;
+
+import com.tidcode.cruddemopart2.entity.Employee;
+
+public interface EmployeeService {
+
+	List<Employee> findAll();
+
+}
