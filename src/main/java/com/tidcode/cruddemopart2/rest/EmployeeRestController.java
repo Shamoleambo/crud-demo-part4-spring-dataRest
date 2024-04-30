e63517ef4a5269584b7e942468cc94e9721fb5d4
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-30 08:30:27 -0300

    add endpoint to save a new employee to the db

--- src/main/java/com/tidcode/cruddemopart2/rest/EmployeeRestController.java
+++ src/main/java/com/tidcode/cruddemopart2/rest/EmployeeRestController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,6 +39,13 @@ public class EmployeeRestController {
 		}
 
 		return employee;
+	}
+
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee employee) {
+		employee.setId(0);
+		Employee dbEmployee = this.employeeService.save(employee);
+		return dbEmployee;
 	}
 
 }
